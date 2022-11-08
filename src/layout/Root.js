1948cdb0255ef38bd816ae8c5e1f62ebccbf9e49
Mubashir Kabir <97.m.kabir@gmail.com>
2022-11-08 20:36:57 +0600

    footer added

--- src/layout/Root.js
+++ src/layout/Root.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Footer } from "../component/Footer";
 import { Navbar } from "../component/Navbar";
 
 const Root = () => {
@@ -7,6 +8,7 @@ const Root = () => {
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
